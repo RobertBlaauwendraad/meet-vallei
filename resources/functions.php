8db7337e27e83262366b6f338e8ba3bebb6b68fd
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2020-02-22 13:53:17 +0100

    Added header

--- resources/functions.php
+++ resources/functions.php
@@ -90,3 +90,11 @@ Container::getInstance()
             'view' => require dirname(__DIR__).'/config/view.php',
         ]);
     }, true);
+    
+/**
+ * Register Custom Navigation Walker
+ */
+function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
