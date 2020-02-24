e31a23929b33bad47cbe155a03027842242b4277
Robert Blaauwendraad <robertblaauwendraad@gmail.com>
2020-02-24 12:42:23 +0100

    Footer design and header redesign

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,13 @@
 <footer class="content-info">
-  <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+  <div class="footer-brands">
+    <div class="footer-mv d-inline-block">
+      <a class="footer-brand" href="{{ home_url('/') }}"><img src="{{ home_url('/wp-content/uploads/2020/02/logoMV.png') }}" alt="Logo MeetVallei" width="50" height="50" class="margin"></a>
+    </div>
+    <div class="footer-zgv d-inline-block">
+      <a class="footer-brand" href="https://www.geldersevallei.nl/home"><img src="{{ home_url('/wp-content/uploads/2020/02/logoZGV.png') }}" alt="Logo MeetVallei" width="50" height="50"></a>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <p>© {{ bloginfo("name") }} 2020</p>
   </div>
 </footer>
